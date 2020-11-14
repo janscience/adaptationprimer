d434e3aec804294f10da941fba8ff0d556213e37
Jan Benda <jan.benda@uni-tuebingen.de>
2020-11-14 16:05:57 +0100

    common plotstyle

--- lifac/lifacplots.py
+++ lifac/lifacplots.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import lifac as la
+import sys
+sys.path.insert(0, '..')
+from plotstyle import figwidth
 
 
 def plot_stimulus(time, stimulus):
     tfac = 1000.0
-    fig, ax = plt.subplots(figsize=(4.0, 2.0))
+    fig, ax = plt.subplots(figsize=(figwidth, 0.4*figwidth))
     ax.plot(tfac*time, stimulus, label=r'$RI(t)$')
     ax.set_ylim(0, 4.5)
     ax.set_xlabel('Time [ms]')
@@ -17,7 +21,7 @@ def plot_stimulus(time, stimulus):
 def plot_trial(time, stimulus):
     tfac = 1000.0             # plots in milliseconds
     spikes, V, A = la.lifac(time, stimulus)
-    fig, ax = plt.subplots(figsize=(4.0, 2.5))
+    fig, ax = plt.subplots(figsize=(figwidth, 0.6*figwidth))
     ax.axhline(0.0, color='k', linestyle=':', label=r'$V_r$')
     ax.axhline(1.0, color='k', linestyle='--', label=r'$\theta$')
     #ax.plot(tfac*time, stimulus, label='$RI(t)$')
@@ -25,8 +29,10 @@ def plot_trial(time, stimulus):
     ax.plot(tfac*time, A, label='$A(t)$')
     ax.eventplot([tfac*spikes], colors=['k'], lineoffsets=1.6)
     ax.set_xlim(tfac*time[0], tfac*time[-1])
+    ax.set_ylim(-1.5, 3.5)
     ax.set_xlabel('Time [ms]')
     ax.set_ylabel('Membrane voltage [mV]')
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1.0))
     ax.legend(ncol=2)
     fig.savefig('lifac-trial')
 
@@ -34,24 +40,17 @@ def plot_trial(time, stimulus):
 def plot_raster(time, spikes):
     tfac = 1000.0             # plots in milliseconds
     spks = [tfac*spikes[k] for k in range(len(spikes))]
-    fig, ax = plt.subplots(figsize=(4.0, 2.5))
+    fig, ax = plt.subplots(figsize=(figwidth, 0.6*figwidth))
     ax.eventplot(spks, colors=['k'], lineoffsets=np.arange(1, len(spks)+1), lw=0.5)
     ax.set_xlim(tfac*time[0], tfac*time[-1])
     ax.set_ylim(0, len(spks)+1)
     ax.set_xlabel('Time [ms]')
     ax.set_ylabel('Trials')
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(5.0))
     fig.savefig('lifac-raster')
 
 
 if __name__ == "__main__":
-    plt.rcParams['savefig.format'] = 'png'
-    plt.rcParams['savefig.dpi'] = 300.0
-    plt.rcParams['figure.constrained_layout.use'] = True
-    plt.rcParams['axes.xmargin'] = 0.0
-    plt.rcParams['axes.ymargin'] = 0.0
-    plt.rcParams['axes.spines.top'] = False
-    plt.rcParams['axes.spines.right'] = False
-    
     time, stimulus = la.step_stimulus(-0.2, 0.8, 0.2, 1.2, 4.0)
     plot_stimulus(time, stimulus)
     plot_trial(time, stimulus)
---
+++ plotstyle.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+figwidth = 3.0
+
+plt.rcParams['savefig.format'] = 'png'
+plt.rcParams['savefig.dpi'] = 300.0
+plt.rcParams['figure.constrained_layout.use'] = True
+plt.rcParams['font.size'] = 5.0
+plt.rcParams['lines.linewidth'] = 0.8
+plt.rcParams['axes.xmargin'] = 0.0
+plt.rcParams['axes.ymargin'] = 0.0
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['legend.frameon'] = False
+plt.rcParams['legend.borderpad'] = 0.0
+
